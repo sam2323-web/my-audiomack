f24f7c6cba7d8efd82deb49ecc2972a207686997
sam2323-web <iyinolasamtex2323@gmail.com>
2026-03-24 23:57:09 -1100

    Add files via upload

---
+++ audiomack.js
@@ -0,0 +1,8 @@
+const trending_parent = document.querySelector(".trending_parent")
+document.querySelector("arrowleft").addEventListener("click", () =>{
+  trending_parent.scroollLeft -=200;
+});
+
+document.querySelector("arrowleft").addEventListener("click", () => {
+    
+]
